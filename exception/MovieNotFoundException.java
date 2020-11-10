5d9e7f7ae8153f8bb8c4e42c700ab7f7e00a3626
99002524 <71481141+99002524@users.noreply.github.com>
2020-11-10 14:55:13 +0530

    Add files via upload

---
+++ exception/MovieNotFoundException.java
@@ -0,0 +1,23 @@
+package com.movie.exception;
+
+ 
+
+public class MovieNotFoundException extends RuntimeException {
+
+ 
+
+    public MovieNotFoundException() {
+        // TODO Auto-generated constructor stub
+    }
+
+ 
+
+    public MovieNotFoundException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+ 
+
+    
+}
